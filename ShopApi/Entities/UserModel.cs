5536ae80d02759f1812fa15c56a60ec6f13e3db9
Anton Kozak <akoz@hms.se>
2023-12-20 13:58:06 +0100

    Add new files

---
+++ ShopApi/Entities/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace ShopApi;
+
+public class UserModel
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    // public string? Password { get; set; }
+    // public string? Email { get; set; }
+    // public string? FirstName { get; set; }
+    // public string? LastName { get; set; }
+}
